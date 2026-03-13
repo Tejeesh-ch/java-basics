39f52c0586f647c1d5b2997aaeeccbee52624cac
Tejeesh-ch <chtejeesh@gmail.com>
2026-03-13 21:46:29 +0530

    Hello World uc1

---
+++ UC1.java
@@ -0,0 +1,15 @@
+/**
+ * HelloApp.java - A simple Java application that displays "Hello, World!" 
+ * on the console.
+ *
+ * @author Developer Name
+ * @version 1.0
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
